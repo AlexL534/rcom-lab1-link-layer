591cc710230a96a92901817dff8cf628c96f656b
Alexandre Lopes <114609590+AlexL534@users.noreply.github.com>
2024-10-18 12:20:25 +0100

    Work done in class, mainly llwrite

---
+++ Redes-Computadores-main/project1/src/application_layer.c
@@ -0,0 +1,15 @@
+// Application layer protocol implementation
+
+#include "application_layer.h"
+
+void applicationLayer(const char *serialPort, const char *role, int baudRate, int nTries, int timeout, const char *filename)
+{
+    LinkLayer linklayer;
+    strcpy(linklayer.serialPort, serialPort);
+    linklayer.baudRate = baudRate;
+    linklayer.nRetransmissions = nTries;
+    linklayer.timeout = timeout;
+    linklayer.role = strcmp(role, "tx") ? LlRx : LlTx;
+
+    llopen(linklayer);
+}
